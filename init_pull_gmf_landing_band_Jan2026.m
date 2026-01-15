ee424dea33fe333d4a323d5aa6a5f018d89305e6
Nick LaSorte <37697540+nicklasorte@users.noreply.github.com>
2026-01-14 20:20:52 -0500

    Add files via upload

---
+++ init_pull_gmf_landing_band_Jan2026.m
@@ -0,0 +1,198 @@
+close all;
+close all force;
+clc;
+app=NaN(1);  %%%%%%%%%This is to allow for Matlab Application integration.
+format shortG
+top_start_clock=clock;
+folder1='C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\4.4GHz';
+cd(folder1)
+addpath(folder1)
+pause(0.1)
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\GMF_functions')
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\Basic_Functions')
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\Census_Functions')
+pause(0.1)
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Pull GMF from 1240mhz to 24 ghz and check assignments for AMT, MM, and LM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%GMF inputs:
+cd(folder1)
+pause(0.1)
+gmf_MinMHz=4940;
+gmf_MaxMHz=24000;
+rev_num=01132026;
+[gmf_table1]=pull_gmf_excel_onedrive_rev2(app,gmf_MinMHz,gmf_MaxMHz,rev_num);
+gmf_header=gmf_table1.Properties.VariableNames;
+cell_gmf1=table2cell(gmf_table1);
+
+gmf_MinMHz=1240;
+gmf_MaxMHz=4940;
+rev_num=01132026;
+[gmf_table2]=pull_gmf_excel_onedrive_rev2(app,gmf_MinMHz,gmf_MaxMHz,rev_num);
+cell_gmf2=table2cell(gmf_table2);
+
+%%%%%%%%%%Merge the GMFs
+cell_gmf=vertcat(cell_gmf1,cell_gmf2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%gmf_header IDX
+col_service_idx=find(matches(gmf_header,'Service'));
+col_agency_idx=find(matches(gmf_header,'Agency'))
+city_col_idx=find(matches(gmf_header,'XAL'))
+col_gmf_ser_idx=find(matches(gmf_header,'SER'))
+col_nts_idx=find(matches(gmf_header,'NTS'));
+col_latdd_idx=find(matches(gmf_header,'XLatDD'));
+col_londd_idx=find(matches(gmf_header,'XLonDD'));
+col_freq1_idx=find(matches(gmf_header,'FRQMHz'));
+col_freq2_idx=find(matches(gmf_header,'FRUMHz'));
+col_ems_idx=find(matches(gmf_header,'EMS'));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Cut
+%%%%%%%%%%%%%Filter out CAN and NG
+ng_idx=find(contains(cell_gmf(:,col_gmf_ser_idx),'NG'));  %%%%%%Non-Government
+cell_gmf(ng_idx,:)=[];
+can_idx=find(contains(cell_gmf(:,col_gmf_ser_idx),'CAN'));   %%%%Cananda
+cell_gmf(can_idx,:)=[];
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Simplifiy the Agency Name in the GMF
+[cell_gmf]=simplify_gmf_agency_name_rev1(app,cell_gmf,gmf_header);
+%%%%%%%%%%Unique Agency Types
+unique(cell_gmf(:,col_agency_idx))
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Unique other strings in the GMF and remove N/A  or NaN
+header_string='XAG';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='XAH';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='STC';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='PWR';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='RAG';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='RAH';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='RAL';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='RSC';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='Service';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+header_string='EMS';
+[cell_gmf]=unique_and_remove_nan_rev1(app,gmf_header,header_string,cell_gmf);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Clean up the Tx EUT name and fill in holes
+[cell_gmf]=clean_gmf_tx_eut_rev1(app,gmf_header,cell_gmf);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Clean up the Rx EUT name and fill in holes
+[cell_gmf]=clean_gmf_rx_eut_rev1(app,gmf_header,cell_gmf);
+
+%%%%%%%%%Clean up the Lat/Lon, if missing --> NAN
+[cell_gmf]=clean_gmf_tx_latlon_rev1(app,gmf_header,cell_gmf);
+
+
+size(cell_gmf)
+
+horzcat(gmf_header',cell_gmf(1,:)')
+unique(cell_gmf(:,col_service_idx))
+
+
+
+%%%%%%%%AMT
+amt_row_idx=find(contains(cell_gmf(:,col_service_idx),'Aeronautical Mobile'));
+cell_gmf_amt=cell_gmf(amt_row_idx,:);
+
+%%%%Cut Aeronautical Mobile-Satellite
+ams_row_idx=find(contains(cell_gmf_amt(:,col_service_idx),'Aeronautical Mobile-Satellite'));
+cell_gmf_amt(ams_row_idx,:)=[];
+size(cell_gmf_amt)
+table_amt_gmf=cell2table(cell_gmf_amt);
+table_amt_gmf.Properties.VariableNames=gmf_header;
+%table_uni_amt_freq=unique(table_amt_gmf(:,[col_freq1_idx,col_freq2_idx,col_service_idx]))%%%%%%%%%%Unique the Frequency Rows
+%%%%%%%Also add the col_agency_idx to the unique table
+table_uni_amt_freq=unique(table_amt_gmf(:,[col_freq1_idx,col_freq2_idx,col_service_idx,col_agency_idx]))%%%%%%%%%%Unique the Frequency Rows
+writetable(table_uni_amt_freq,strcat('table_uni_amt_freq.xlsx'));
+
+
+
+'How to compile the frequencies into bands?'
+'Maybe pull excel data of table allocation for allocation lines.'
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%MM
+mm_row_idx=find(contains(cell_gmf(:,col_service_idx),'Maritime Mobile'));
+cell_gmf_mm=cell_gmf(mm_row_idx,:);
+
+%%%%%%%%%%% Cut Maritime Mobile-Satellite
+mms_row_idx=find(contains(cell_gmf_mm(:,col_service_idx),'Maritime Mobile-Satellite'));
+cell_gmf_mm(mms_row_idx,:)=[];
+
+size(cell_gmf_mm)
+table_mm_gmf=cell2table(cell_gmf_mm);
+table_mm_gmf.Properties.VariableNames=gmf_header;
+table_uni_mm_freq=unique(table_mm_gmf(:,[col_freq1_idx,col_freq2_idx,col_service_idx,col_agency_idx])) %%%%%%%%%%Unique the Frequency Rows
+writetable(table_uni_mm_freq,strcat('table_uni_mm_freq.xlsx'));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%LM
+landmobile_row_idx=find(contains(cell_gmf(:,col_service_idx),'Land Mobile'));
+cell_gmf_land=cell_gmf(landmobile_row_idx,:);
+
+%%%%%%Cut Land Mobile-Satellite
+landsat_row_idx=find(contains(cell_gmf_land(:,col_service_idx),'Land Mobile-Satellite'));
+cell_gmf_land(landsat_row_idx,:)=[];
+
+size(cell_gmf_land)
+table_land_gmf=cell2table(cell_gmf_land);
+table_land_gmf.Properties.VariableNames=gmf_header;
+table_uni_land_freq=unique(table_land_gmf(:,[col_freq1_idx,col_freq2_idx,col_service_idx,col_agency_idx])) %%%%%%%%%%Unique the Frequency Rows
+writetable(table_uni_land_freq,strcat('table_uni_land_freq.xlsx'));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Fixed
+fixed_row_idx=find(contains(cell_gmf(:,col_service_idx),'Fixed'));
+cell_gmf_fixed=cell_gmf(fixed_row_idx,:);
+
+%%%%%%Cut 'Fixed-Satellite'
+fixedsat_row_idx=find(contains(cell_gmf_fixed(:,col_service_idx),'Fixed-Satellite'));
+cell_gmf_fixed(fixedsat_row_idx,:)=[];
+size(cell_gmf_fixed)
+table_fixed_gmf=cell2table(cell_gmf_fixed);
+table_fixed_gmf.Properties.VariableNames=gmf_header;
+table_uni_fixed_freq=unique(table_fixed_gmf(:,[col_freq1_idx,col_freq2_idx,col_service_idx,col_agency_idx])) %%%%%%%%%%Unique the Frequency Rows
+writetable(table_uni_fixed_freq,strcat('table_uni_fixed_freq.xlsx'));
+
+
+
+
+
+end_clock=clock;
+total_clock=end_clock-top_start_clock;
+total_seconds=total_clock(6)+total_clock(5)*60+total_clock(4)*3600+total_clock(3)*86400;
+total_mins=total_seconds/60;
+total_hours=total_mins/60;
+if total_hours>1
+    strcat('Total Hours:',num2str(total_hours))
+elseif total_mins>1
+    strcat('Total Minutes:',num2str(total_mins))
+else
+    strcat('Total Seconds:',num2str(total_seconds))
+end
+
+'DONE'
+cd(folder1)
